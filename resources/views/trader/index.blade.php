a470315d7d699ce0accc6fc67f6e1dbcd8fa0de8
Samyak Maharjan <samyak.maharjan@debajyotigroup.com>
2020-05-31 14:48:49 +0545

    add trader dashboard

--- resources/views/trader/index.blade.php
+++ resources/views/trader/index.blade.php
@@ -1,75 +1,103 @@
 @extends('trader.layouts.master')
 @section('title','Dashboard')
-@section('content')
-    <!--breadcrumbs-->
-    <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
-        
+@section('addCss')
+    <style>
+        .dash-main{
+            padding-top: 10%;
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            max-width: 900px;
+            margin:0 auto;
+        }
 
-    <!--End-breadcrumbs-->
-
-    <!--Action boxes-->
-    <div class="container-fluid">
-        <div class="quick-actions_homepage">
-            <ul class="quick-actions">
-                <li class="bg_lb"> <a href="index.html"> <i class="icon-dashboard"></i> <span class="label label-important">20</span> My Dashboard </a> </li>
-                <li class="bg_lg span3"> <a href="charts.html"> <i class="icon-signal"></i> Charts</a> </li>
-                <li class="bg_ly"> <a href="widgets.html"> <i class="icon-inbox"></i><span class="label label-success">101</span> Widgets </a> </li>
-                <li class="bg_lo"> <a href="tables.html"> <i class="icon-th"></i> Tables</a> </li>
-                <li class="bg_ls"> <a href="grid.html"> <i class="icon-fullscreen"></i> Full width</a> </li>
-                <li class="bg_lo span3"> <a href="form-common.html"> <i class="icon-th-list"></i> Forms</a> </li>
-                <li class="bg_ls"> <a href="buttons.html"> <i class="icon-tint"></i> Buttons</a> </li>
-                <li class="bg_lb"> <a href="interface.html"> <i class="icon-pencil"></i>Elements</a> </li>
-                <li class="bg_lg"> <a href="calendar.html"> <i class="icon-calendar"></i> Calendar</a> </li>
-                <li class="bg_lr"> <a href="error404.html"> <i class="icon-info-sign"></i> Error</a> </li>
-
-            </ul>
-        </div>
-    </div>
-@endsection
-@section('jsblock')
-    <script src="{{asset('js/excanvas.min.js')}}"></script>
-    <script src="{{asset('js/jquery.min.js')}}"></script>
-    <script src="{{asset('js/jquery.ui.custom.js')}}"></script>
-    <script src="{{asset('js/bootstrap2.min.js')}}"></script>
-    <script src="{{asset('js/jquery.flot.min.js')}}"></script>
-    <script src="{{asset('js/jquery.flot.resize.min.js')}}"></script>
-    <script src="{{asset('js/jquery.peity.min.js')}}"></script>
-    <script src="{{asset('js/fullcalendar.min.js')}}"></script>
-    <script src="{{asset('js/matrix.js')}}"></script>
-    <script src="{{asset('js/matrix.dashboard.js')}}"></script>
-    <script src="{{asset('js/jquery.gritter.min.js')}}"></script>
-    <script src="{{asset('js/matrix.interface.js')}}"></script>
-    <script src="{{asset('js/matrix.chat.js')}}"></script>
-    <script src="{{asset('js/jquery.validate.js')}}"></script>
-    <script src="{{asset('js/jquery.wizard.js')}}"></script>
-    <script src="{{asset('js/jquery.uniform.js')}}"></script>
-    <script src="{{asset('js/select2.min.js')}}"></script>
-    <script src="{{asset('js/matrix.popover.js')}}"></script>
-    <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('js/matrix.tables.js')}}"></script>
-    <script src="{{asset('js/matrix.form_validation.js')}}"></script>
-    <script type="text/javascript">
-        // This function is called from the pop-up menus to transfer to
-        // a different page. Ignore if the value returned is a null string:
-        function goPage (newURL) {
-
-            // if url is empty, skip the menu dividers and reset the menu selection to default
-            if (newURL != "") {
-
-                // if url is "-", it is this page -- reset the menu:
-                if (newURL == "-" ) {
-                    resetMenu();
-                }
-                // else, send page to designated URL
-                else {
-                    document.location.href = newURL;
-                }
+        @media (max-width: 700px){
+            .dash-main{
+                justify-content: center;
             }
         }
 
-        // resets the menu selection upon entry to this page:
-        function resetMenu() {
-            document.gomenu.selector.selectedIndex = 2;
+        .card {
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+            transition: 0.3s;
+            max-width: 250px;
+            margin-top: 15px;
+            background-color: #34BC6D;
         }
-    </script>
+
+        .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        }
+
+        .containerrr {
+            text-align: center;
+            padding: 2px 16px;
+            color: white;
+        }
+
+        .containerrr div{
+            height: 200px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 5.5em;
+        }
+    </style>
+@endsection
+@section('content')
+<?php
+    $shopId = Auth::user()->shop->id;
+    $productCount = App\Product::where('shop_id', $shopId)->count();
+
+    $itemsSold = 0;
+    $earnings = 0;
+    $orders = App\Order::where('delivery_status', 1)->get();
+    foreach($orders as $order){
+        $orderProducts = App\OrderProduct::where('order_id', $order->id)->get();
+        foreach($orderProducts as $value){
+            $product = App\Product::where('id', $value->product_id)->get();
+            // dd($product);
+            if($product[0]->shop_id == $shopId){
+                $price = $product[0]->price * $value->quantity;
+                $earnings += $price;
+                $itemsSold+=$value->quantity;
+            }
+        }
+    }
+
+
+?>
+<div class = 'dash-main'> 
+    <div class="card">
+        <div class="containerrr">
+            <h3><b>Total Products</b></h3> 
+            <div>{{$productCount}}</div> 
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="containerrr">
+            <h3><b>Items Sold</b></h3> 
+            <div>{{$itemsSold}}</div> 
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="containerrr">
+            <h3><b>Total Earnings(&pound;)</b></h3> 
+            <div>{{$earnings}}</div> 
+        </div>
+    </div>
+</div>
+@endsection
+@section('jsblock')
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/jquery.ui.custom.js')}}"></script>
+    <script src="{{asset('js/bootstrap2.min.js')}}"></script>
+    <script src="{{asset('js/jquery.uniform.js')}}"></script>
+    <script src="{{asset('js/select2.min.js')}}"></script>
+    <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('js/matrix.js')}}"></script>
+    <script src="{{asset('js/matrix.tables.js')}}"></script>
+    <script src="{{asset('js/matrix.popover.js')}}"></script>
 @endsection
